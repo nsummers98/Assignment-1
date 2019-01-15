46e6198d23ed0dd3ef19cf878e74c49c0ee22ea9
Nathan Summers <nsummers@ucla.edu>
2019-01-15 15:57:55 -0800

    IO Initial Work
    Did some basic input/output stuff so as to have something to test

--- Assignment 1/siete-y-medio.cpp
+++ Assignment 1/siete-y-medio.cpp
@@ -18,6 +18,25 @@ using namespace std;
 
 // Stub for main
 int main() {
-	/* --STATEMENTS-- */
+	Player p1(100);
+
+	while (p1.get_balance() > 0)
+	{
+		Hand playerHand;
+		Hand dealerHand;
+		cout << "You have $" << p1.get_balance() << ". Enter bet: ";
+
+		int bet;
+		cin >> bet;
+
+		cout << "Your cards:" << endl;
+		cout << "	" << /*Accessor Function*/ "        (" << /*Accessor Function*/ ")." << endl;
+		cout << "Your total is " << playerHand.get_value() << ". Do you want another card? (y/n) ";
+		cout << "Dealer's cards: " << endl;
+		cout << "	" << /*Accessor Function*/ "        (" << /*Accessor Function*/ ")." << endl;
+		cout << "The dealer's total is " << dealerHand.get_value() << "." << endl;
+	}
+	
+
 	return 0;
 }
