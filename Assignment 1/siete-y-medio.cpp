dc89dd15c27c399826a12443d6c4e8f2d8881416
Nathan Summers <nsummers@ucla.edu>
2019-01-17 15:28:21 -0800

    Player Bust Message
    Implemented a message to alert players when they went over 7.5

--- Assignment 1/siete-y-medio.cpp
+++ Assignment 1/siete-y-medio.cpp
@@ -30,11 +30,11 @@ int winner(Hand h1, Hand h2)
 
 void waitForInput()
 {
-	std::cin.ignore();
 	do
 	{
-		std::cout << "\nPress enter key to continue . . .";
-	} while (std::cin.get() != '\n');
+		cout << "\nPress enter key to continue . . .";
+	} while (cin.get() != '\n');
+	cin.ignore();
 }
 
 
@@ -43,11 +43,13 @@ int main() {
 	Player p1(100);
 	int gamecount = 1;
 
-	while (p1.get_balance() > 0 && p1.get_balance() < 999)
+	while (p1.get_balance() > 0 && p1.get_balance() < 900)
 	{
 		Hand playerHand;
 		Hand dealerHand;
 
+		if (gamecount > 1)
+			cout << "\n";
 		cout << "          Game " << gamecount << endl;
 
 		int bet;
@@ -65,7 +67,16 @@ int main() {
 			cout << "Your total is " << playerHand.get_value() << ". Do you want another card? (y/n) ";
 			cin >> res;
 			if (res == 'y' || res == 'Y')
+			{
 				playerHand.deal_card();
+				if (playerHand.get_value() > 7.5)
+				{
+					cout << "Bust!" << endl;
+					cout << "Your total is " << playerHand.get_value() << endl;
+					waitForInput();
+				}
+			}
+			else break;
 		}
 
 		int n = 0;
@@ -73,13 +84,14 @@ int main() {
 		{
 			if (n != 0)
 				dealerHand.deal_card();
-			cout << "\n\nDealer's cards: " << endl;
+			cout << "\nDealer's cards: " << endl;
 			cout << dealerHand.print_hand();
-			cout << "The dealer's total is " << dealerHand.get_value() << "." << endl;
+			cout << "\nThe dealer's total is " << dealerHand.get_value() << "." << endl;
 			++n;
 		}
 
 		int win = winner(playerHand, dealerHand);
+		cout << endl;
 		if (win == 0)
 		{
 			cout << "You win $" << bet << ". Congratulations.\n" << endl;
@@ -98,14 +110,17 @@ int main() {
 		++gamecount;
 	}
 	
-	if (p1.get_balance() > 0)
+	if (p1.get_balance() <= 0)
 	{
-
+		cout << "You're broke! GAME OVER!" << endl;
+		cout << "Come back when you have more money." << endl;
 	}
-	else if (p1.get_balance() < 999)
+	else if (p1.get_balance() >= 900)
 	{ 
-
+		cout << "Congratulations! You ran the house!" << endl;
 	}
+
+	cout << "\nBye!" << endl;
 
 	return 0;
 }
